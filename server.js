10edcde488ad5fd07b09a6baea0d769e4fb69fed
EyderACM <eyderacm@gmail.com>
2019-06-08 17:18:40 -0500

    Connect routes to server

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
