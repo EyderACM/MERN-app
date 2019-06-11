77f8f3686db4945c36bca20fffbf622d9c430050
EyderACM <eyderacm@gmail.com>
2019-06-11 10:35:50 -0500

    Fix profile auth problem

--- models/Profile.js
+++ models/Profile.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const profileSchema = new mongoose.Schema({
+const ProfileSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
   company: { type: String },
   website: { type: String },
@@ -41,4 +41,4 @@ const profileSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now }
 });
 
-module.exports = Profile = mongoose.model('profile', profileSchema);
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
