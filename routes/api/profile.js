eb360f35d1ddd7b27d9c878d56951280fd4b9b28
EyderACM <eyderacm@gmail.com>
2019-06-11 21:18:19 -0500

    fix duplicated header

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -358,7 +358,7 @@ router.get('/github/:username', (req, res) => {
       if (error) console.error(error);
 
       if (response.statusCode !== 200) {
-        res.status(404).json({
+        return res.status(404).json({
           msg: 'No github profile found'
         });
       }
