5dc8090db809d209e7fdb7e7365818fbdb3adaea
EyderACM <eyderacm@gmail.com>
2019-06-12 15:53:12 -0500

    Unlike routes functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -133,4 +133,33 @@ router.put('/like/:id', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: `This post hasn't been liked yet` });
+    }
+
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.params.id);
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
