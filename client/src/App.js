1d38d250c3c9a38a34a5bbe8d42656ef80e15109
EyderACM <eyderacm@gmail.com>
2019-06-17 15:41:17 -0500

    Replace with tsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import React, { Fragment } from 'react';
+import * as React from 'react';
 
-const App = () => (
-  <Fragment className='App'>
-    <h1>App</h1>
-  </Fragment>
-);
-
-export default App;
+export default class App extends React.Component {
+  render() {
+    return <h1>Stuff</h1>;
+  }
+}
