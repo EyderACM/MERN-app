bf5287a221ab666b625b40bcda57f959b2c526bc
EyderACM <eyderacm@gmail.com>
2019-06-18 23:13:11 -0500

    Make mobile responsive

--- client/src/Views/LandingPage/components/ImageWrapper.tsx
+++ client/src/Views/LandingPage/components/ImageWrapper.tsx
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const ImageWrapper = styled.div`
   align-items: center;
   display: flex;
-  height: 20rem;
+  height: 18rem;
   justify-content: center;
 `;
--- client/src/Views/LandingPage/components/LandingButtonWrapper.tsx
+++ client/src/Views/LandingPage/components/LandingButtonWrapper.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 export const LandingButtonWrapper = styled.div`
+  align-items: center
   display: flex;
   flex-direction: column;
   height: 120px;
   justify-content: space-around;
-  margin: 50px 0;
+  margin-top: 3vh;
 `;
