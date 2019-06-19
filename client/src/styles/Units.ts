bd98ee86355456f297e31bfdf2aaf3e380b44875
EyderACM <eyderacm@gmail.com>
2019-06-19 15:13:45 -0500

    Add units

--- client/src/styles/Units.ts
+++ client/src/styles/Units.ts
@@ -1,3 +1,11 @@
+// Screen sizes
+export const MIN_TABLET_WIDTH = '768px';
+export const MAX_TABLET_WIDTH = '1023px';
+export const MIN_BIG_MOBILE_WIDTH = '426px';
+export const MIN_DESKTOP_WIDTH = '1024px';
+
 // Images
 export const DESKTOP_IMAGE = '450px';
 export const MOBILE_IMAGE = '250px';
+
+
