e6575872edf6c0b0b5bbee8f0c3526edb3feafcd
EyderACM <eyderacm@gmail.com>
2019-06-20 16:11:19 -0500

    Add login wrapper

---
+++ client/src/Views/Login/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { LoginPageWrapper } from './components/LoginPageWrapper';
+import { H1 } from '../../components/ui/H1/H1';
+
+export const LoginPage = () => (
+  <LoginPageWrapper>
+    <H1 color='primary'>Testing</H1>
+  </LoginPageWrapper>
+);
---
+++ client/src/Views/Login/components/LoginPageWrapper.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const LoginPageWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+`;
